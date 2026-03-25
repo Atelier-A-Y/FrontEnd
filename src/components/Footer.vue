3f38a31a1b3fe70498b4a49803909010f46cbe12
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2026-03-25 16:41:54 -0300

    feat: criação do css do footer e do caminho para o instagram #3

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,29 +4,62 @@
             <section>
                 <h1>Central de Ajuda</h1>
                 <p>Seg. a Sex. das 8:00 às 16:00</p>
-                <p><img src="/img/icons8-phone-50.png" alt="" width="15" height="15">(47) 7680-6642</p>
+                <p><span><img src="/img/telefone.png" alt="telefone" width="15" height="15"></span>(47) 7680-6642</p>
                 <p><span class="fa-brands fa-whatsapp"></span>(47) 94577-9812</p>
             </section>
             <section>
                 <h1>Redes Sociais</h1>
-                <p><span class="fa-brands fa-instagram"></span>@_a.y.atelier_</p>
+                <p><span class="fa-brands fa-instagram"></span><a href="https://www.instagram.com/_a.y.atelier_/">@_a.y.atelier_</a></p>
                 <p><span class="fa-brands fa-x-twitter"></span>@a.y__atelier</p>
             </section>
         </div>
-        <p> 17.296.164/0002-55 | Rua Luiz Maski, 848, Bloco 2 | Itoupavazinha - 89096-650 - Blumenau - SC</p>
+        <p class="endereço"> 17.296.164/0002-55 | Rua Luiz Maski, 848, Bloco 2 | Itoupavazinha - 89096-650 - Blumenau -
+            SC</p>
     </footer>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Klee+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Klee+One&display=swap');
+
 footer {
     background-color: #F5E9E0;
     text-align: center;
-    padding: 1vw 29.4vw;
+    width: 100%;
+    padding-top: 2vw;
 }
+
 div {
     display: flex;
-    justify-content: space-around;
     color: rgb(5, 5, 5);
     margin-bottom: 50px;
+    justify-content: space-around;
+    font-family: 'Klee One', cursive;
+}
+
+div h1 {
+    font-family: 'Julius Sans One', sans-serif;
+    font-size: 2.5rem;
+}
+
+div p {
+    text-align: center;
+    font-size: 1.2rem;
+}
+
+span {
+    padding: 0 6px 0 0;
+}
+
+a {
+    color: rgb(5, 5, 5);
+    text-decoration: none;
+}
+
+.endereço {
+    background: #311111;
+    color: white;
+    text-align: center;
+    padding: 2vw;
 }
 </style>
