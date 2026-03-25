2df98340cb329f460346c94680b04ee5556ef41b
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2026-03-25 11:24:35 -0300

    feat: criação do conteúdo do footer #3

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+        <div>
+            <section>
+                <h1>Central de Ajuda</h1>
+                <p>Seg. a Sex. das 8:00 às 16:00</p>
+                <p><img src="/img/icons8-phone-50.png" alt="" width="15" height="15">(47) 7680-6642</p>
+                <p><span class="fa-brands fa-whatsapp"></span>(47) 94577-9812</p>
+            </section>
+            <section>
+                <h1>Redes Sociais</h1>
+                <p><span class="fa-brands fa-instagram"></span>@_a.y.atelier_</p>
+                <p><span class="fa-brands fa-x-twitter"></span>@a.y__atelier</p>
+            </section>
+        </div>
+        <p> 17.296.164/0002-55 | Rua Luiz Maski, 848, Bloco 2 | Itoupavazinha - 89096-650 - Blumenau - SC</p>
+    </footer>
+</template>
+
+<style>
+footer {
+    background-color: #F5E9E0;
+    text-align: center;
+    padding: 1vw 29.4vw;
+}
+div {
+    display: flex;
+    justify-content: space-around;
+    color: rgb(5, 5, 5);
+    margin-bottom: 50px;
+}
+</style>
