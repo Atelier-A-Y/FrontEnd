579d5b553c60c4be2e8e8f572d4ac68ef6a6a05f
analaura3s <analauras.ifc.info3@gmail.com>
2026-03-25 11:09:16 -0300

    feat: Criação do Header #1

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <header>
+
+        <img src="/img/android-chrome-512x512.png" alt="">
+
+    </header>
+
+</template>
+
+<style scoped>
+
+header{
+    color: #F5E9E0;
+    position: fixed;
+    
+}
+
+</style>
