dc16b6919bf48c31e369e5c177546a42eb300e32
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-03-26 22:21:18 -0300

    Feat: continuação do carrossel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,17 @@
 import { ref } from 'vue'
 
 const slides = [
-  '/img/noiva1.png',
-  '/img/formanda1.png'
+  {
+   img: '/img/noiva1.png',
+   marca1: 'ATELIER',
+   marca2: 'A.Y.',
+   texto: 'CASAMENTO',
+   subtexto: 'NEW EUROPE COLLECTION'
+  },
+  {
+   img: '/img/formanda1.png',
+   texto: 'FORMATURA'
+  }
 ]
 
 const index = ref(0)
@@ -16,21 +25,37 @@ function prevSlide() {
   index.value = (index.value - 1 + slides.length) % slides.length
 }
 
-setInterval(nextSlide, 3000)
+setInterval(nextSlide, 4000)
 </script>
 
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
   <main>
     <div class="carrossel-principal">
-      <img
-        v-for="(img, i) in slides"
-        :key="i"
-        :src="img"
-        :class="{ active: i === index }"
-      />
-      <button @click="prevSlide">❮</button>
-      <button @click="nextSlide">❯</button>
+  <img
+    v-for="(slide, i) in slides"
+    :key="i"
+    :src="slide.img"
+    :class="{ active: i === index }"
+  />
+
+  <h2>
+    <div class="marca1">
+      {{ slides[index].marca1 }}
     </div>
+    <div class="marca2">
+      {{ slides[index].marca2 }}
+    </div>
+    <div class="texto">
+      {{ slides[index].texto }}
+    </div>
+    <div class="subtexto">
+      {{ slides[index].subtexto }}
+    </div>
+  </h2>
+  <button @click="prevSlide">❮</button>
+  <button @click="nextSlide">❯</button>
+</div>
   </main>
 </template>
 
@@ -47,6 +72,14 @@ setInterval(nextSlide, 3000)
   justify-content: center;
 }
 
+.carrossel-principal::after {
+  content: "";
+  position: absolute;
+  inset: 0;
+  background: rgba(214, 168, 140, 0.479);
+  z-index: 1;
+}
+
 img {
   position: absolute;
   width: 100%;
@@ -54,6 +87,7 @@ img {
   object-fit: cover;
   opacity: 0;
   transition: opacity 0.5s ease;
+  z-index: 0;
 }
 
 img.active {
@@ -69,8 +103,79 @@ button {
   border: none;
   padding: 10px;
   cursor: pointer;
+  z-index: 2;
 }
 
 button:first-of-type { left: 10px; }
 button:last-of-type { right: 10px; }
+
+.slide {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+}
+
+.slide.active {
+  opacity: 1;
+}
+
+.slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.texto {
+  position: absolute;
+  bottom: 42%;
+  right: 50%;
+  color: white;
+  font-size: 6rem;
+  z-index: 2;
+  font-family: "Lexend Deca", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  letter-spacing: 4px;
+  text-shadow: 5px 0px 0px #f8ceac;
+}
+
+.subtexto{
+  position: absolute;
+  bottom: 40%;
+  right: 58%;
+  color: #ffeddf;
+  font-size: 2rem;
+  z-index: 2;
+  font-family: "Nanum Myeongjo", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.marca1{
+  position: absolute;
+  bottom: 64%;
+  right: 68%;
+  color: rgb(255, 255, 255);
+  font-size: 2rem;
+  z-index: 2;
+  font-family: "Noto Rashi Hebrew", serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  text-shadow: 3.5px 0px 0px #eec19c;
+}
+
+.marca2{
+  position: absolute;
+  bottom: 60%;
+  right: 71%;
+  color: white;
+  font-size: 1.7rem;
+  z-index: 2;
+  font-family: "Noto Rashi Hebrew", serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  text-shadow: 3.5px 0px 0px #eec19c;
+}
 </style>
