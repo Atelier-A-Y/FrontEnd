39b4a5366410e6a447c3b64b8bb222b289119f25
analaura3s <analauras.ifc.info3@gmail.com>
2026-03-27 16:09:28 -0300

    feat: Lado esquerdo do Header #1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,41 @@
 
     <header class="header">
 
-        <img src="/img/android-chrome-512x512.png" width="75" height="75" alt="">
+        <ul class="esquerdo">
+
+        <li class="logo">
+        
+        <img src="/public/img/Captura de tela 2026-03-04 210312 1 (1) 1@2x (1).png" width="85" height="85" alt="">
+
+        </li>
+
+        <ul class="texto">
+        
+        <li class="titulo">
+        
+        Atelier
+
+        </li>
+
+        <li class="sigla">
+        
+            A.Y.
+        
+        </li>
+        
+        </ul>
+
+        </ul>
+
+        <ul class="direito">
+        
+            <li class="globo">
+            
+            <button></button>
+            
+            </li>
+        
+        </ul>
 
     </header>
 
@@ -19,6 +53,60 @@
   background: #F5E9E0;
   width: 100%;
   padding-left: 2.5vw;
+}
+
+.logo{
+
+margin-top: 0.9vw;
+
+}
+
+.esquerdo{
+
+display: flex;
+list-style-type: none;
+
+}
+
+.texto{
+
+list-style-type: none;
+margin-top: 1.3vw;
+margin-bottom: 1vw;
+color: #84453d;
+font-family: 'Playfair Display', serif;
+margin-left: -2vw;
+
+}
+
+.titulo{
+
+font-size: 2rem;
+color: #84453d;
+font-family: 'Libre Bodoni', serif;
+font-weight: 100;
+letter-spacing: 0.1rem;
+list-style-type: none;
+
+}
+
+.sigla{
+
+font-size: 1.2rem;
+color: #84453d;
+font-family: 'Libre Bodoni', serif;
+font-weight: 100;
+letter-spacing: 0.11em;
+list-style-type: none;
+margin-top: -1vw;
+padding-left: 1.8vw;
+
+}
+
+.direito{
+
+list-style-type: none;
+
 }
 
 </style>
