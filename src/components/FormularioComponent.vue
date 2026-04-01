d8a0efe43aad4262271d12f4141981eaddbbae2a
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-04-01 16:46:22 -0300

    Feat: continuação do formulário #4

--- src/components/FormularioComponent.vue
+++ src/components/FormularioComponent.vue
@@ -4,45 +4,97 @@ import { ref } from 'vue'
 const email = ref('');
 const senha = ref('')
 
-function enviarFormulario(){
+function entrar(){
   console.log({ Email: email.value, Senha: senha.value})
 }
 </script>
 
 <template>
-  <form @submit.prevent="enviarFormulario">
+  <form @submit.prevent="entrar">
+    <div class="fundo">
 
-    <div for="email">
-      <input
-        id="email"
-        type="email"
-        v-model="email"
-        placeholder="Digite seu email..."
-      />
+    <img src="/public/css/modelo2.png" alt="bg modelo2">
+    <img src="/public/css/modelo3.png" alt="bg modelo3">
+    <img src="/public/css/modelo5.png" alt="bg modelo5">
+    <img src="/public/css/modelo8.png" alt="bg modelo8">
+    <img src="/public/css/modelo10.png" alt="bg modelo10">
+    <img src="/public/css/modelo11.png" alt="bg modelo11">
+    <img src="/public/css/modelo12.png" alt="bg modelo12">
+    <img src="/public/css/modelo13.png" alt="bg modelo13">
+
+      <div class="formulario">
+        <div for="email">
+          <input
+            id="email"
+            type="email"
+            v-model="email"
+            placeholder="Digite seu email..."
+          />
+        </div>
+
+        <div for="senha">
+          <input
+            id="senha"
+            type="password"
+            v-model="senha"
+            placeholder="Digite sua senha..."
+          />
+        </div>
+
+        <button type="submit">Entrar</button>
+      </div>
     </div>
-
-    <div for="senha">
-      <input
-        id="senha"
-        type="text"
-        v-model="senha"
-        placeholder="Digite sua senha..."
-      />
-    </div>
-
-    <button type="submit">Entrar</button>
   </form>
 </template>
 
 <style scoped>
 
-form{
-  max-width: 400px;
-  margin: 20px auto;
-  padding: 80px;
-  border-radius: 10px;
+.fundo {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background: #cfc5bc;
+}
+
+.bg {
+  position: absolute;
+  object-fit: cover;
+}
+
+.modelo2 {
+  width: 300px;
+  top: -50px;
+  left: -80px;
+}
+
+.modelo3 {
+  width: 350px;
+  top: 100px;
+  right: -100px;
+}
+
+.modelo5 {
+  width: 280px;
+  bottom: -60px;
+  left: 100px;
+}
+
+.modelo8 {
+  width: 320px;
+  bottom: 0;
+  right: 50px;
+}
+
+.overlay {
+  position: absolute;
+  inset: 0;
+  background: rgba(0,0,0,0.4);
+}
+
+.formulario{
   background-color: #F5E9E0;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  padding: 20vw;
 }
 
 div {
@@ -65,16 +117,10 @@ input {
   transition: border 0.2s, box-shadow 0.2s;
 }
 
-input:focus,
-textarea:focus {
+input:focus{
   border-color: #3b82f6;
   outline: none;
   box-shadow: 0 0 0 2px rgba(59, 130, 246, 0.2);
-}
-
-textarea {
-  min-height: 100px;
-  resize: vertical;
 }
 
 button {
