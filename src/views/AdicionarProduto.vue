5799f2348340f696e4251c854339a1e48808037b
analaura3s <analauras.ifc.info3@gmail.com>
2026-04-29 16:48:31 -0300

    FEAT: início

--- src/views/AdicionarProduto.vue
+++ src/views/AdicionarProduto.vue
@@ -4,6 +4,33 @@
 
 <h1>Adicionar Produto</h1>
 
+<div class="fundo">
+
+
+
+</div>
+
 </main>
 
 </template>
+
+<style scoped>
+
+h1{
+
+color: #311111;
+font-weight: bold;
+margin-top: 3vw;
+margin-bottom: 3vw;
+text-align: center;
+
+}
+
+.fundo{
+
+background-color: #F5E9E0;
+border: #311111 solid 0.8px;
+
+}
+
+</style>
