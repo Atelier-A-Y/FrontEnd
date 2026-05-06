7af28fbaa21217317a8bb745ea71daafef475df1
analaura3s <analauras.ifc.info3@gmail.com>
2026-05-06 16:43:23 -0300

    FEAT: Finalizado

--- src/views/AdicionarProduto.vue
+++ src/views/AdicionarProduto.vue
@@ -6,9 +6,8 @@
 
 <section class="fundo">
 
-<div class="form-container">
+<form class="form-container">
 
-  <!-- VARCHAR -->
   <div class="input-group">
     <input type="text" maxlength="100" placeholder="Categoria">
   </div>
@@ -21,7 +20,6 @@
     <input type="text" maxlength="100" placeholder="Nome">
   </div>
 
-  <!-- VARCHAR menor -->
   <div class="input-group">
     <input type="text" maxlength="10" placeholder="Tamanho">
   </div>
@@ -30,22 +28,20 @@
     <input type="text" maxlength="30" placeholder="Cor">
   </div>
 
-  <!-- DECIMAL -->
   <div class="input-group">
     <input type="number" step="0.01" placeholder="Preço">
   </div>
 
-  <!-- TEXT -->
   <div class="input-group">
     <textarea placeholder="Descrição"></textarea>
   </div>
 
   <div class="buttons">
-    <button type="button">Excluir</button>
+    <button type="reset">Excluir</button>
     <button type="submit">Salvar</button>
   </div>
 
-</div>
+</form>
 
 </section>
 
@@ -69,21 +65,28 @@ text-align: center;
 
 background-color: #F5E9E0;
 border: #311111 solid 0.8px;
+border-radius: 1.7rem;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 40px 30px 40px 30px;
+margin: 0vw 13vw 1.2vw 13vw;
 
 }
 
-body {
-    font-family: Arial, sans-serif;
-  }
+.form-container {
+  width: 350px;
+}
 
-  .form-container {
-    width: 350px;
-    margin: 20px;
-  }
+ .input-group {
+  margin-bottom: 15px;
+}
 
-  .input-group {
-    margin-bottom: 15px;
-  }
+input,
+textarea,
+button {
+  font-family: Arial, sans-serif;
+}
 
   input, textarea {
     width: 100%;
@@ -121,7 +124,7 @@ body {
   }
 
   button:hover {
-    background-color: #f1f1f1;
+    background-color: #e7e7e7;
   }
 
 </style>
