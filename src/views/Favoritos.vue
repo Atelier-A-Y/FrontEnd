d1a8627007816712f35d62bf875f63d8d6b98fdb
analaura3s <analauras.ifc.info3@gmail.com>
2026-05-27 11:04:24 -0300

    FEAT: Criação da página favoritos

---
+++ src/views/Favoritos.vue
@@ -0,0 +1,76 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+
+<main>
+
+<section class="fundo">
+
+<ul>
+
+<li class="icone">
+
+
+
+</li>
+
+<li class="titulo">
+
+<h1>Favoritos Vazio</h1>
+
+</li>
+
+<li class="texto">
+
+<p>Que tal olhar nossos produtos e receber seu pedido na porta de casa?</p>
+
+</li>
+
+</ul>
+
+</section>
+
+</main>
+
+</template>
+
+<style scoped>
+
+ul{
+
+list-style-type: none;
+
+}
+
+h1{
+
+justify-content: center;
+color: #311111;
+padding: 0vw 5vw 1vw 12vw;
+
+}
+
+p{
+
+color: black;
+padding: 0vw 4vw 0vw 5vw;
+
+}
+
+.fundo {
+  background-color: #f5e9e0;
+  border: #311111 solid 0.8px;
+  border-radius: 1.7rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 30px;
+  margin: 0vw 13vw 1.2vw 13vw;
+
+}
+
+</style>
