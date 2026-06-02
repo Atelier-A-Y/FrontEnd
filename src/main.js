e7f1951adf71512969ad57c7d2855d1b98682e05
agathahelena <agatha.helena.so@gmail.com>
2026-06-02 16:36:59 -0300

    FEAT : instalação do docker e esta funcionando muito bem

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8080/api'
 
 const app = createApp(App)
 app.use(router)
